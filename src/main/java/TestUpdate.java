c7a66f02732bb7d9d8f7d46554a43234463dde05
HCX <985701799@qq.com>
2018-12-24 11:42:55 +0800

    1、增加TestUpdate

---
+++ src/main/java/TestUpdate.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName TestUpdate
+ * @Description 改变
+ * @Author HCX
+ * @Date 2018/12/24 11:36
+ * @Version 1.0
+ **/
+public class TestUpdate {
+    public static void main(String[] args) {
+        System.out.println("update test!");
+    }
+}
