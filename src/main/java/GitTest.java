bfe04caa424344bad6e76a40c673c073f629523c
HCX <985701799@qq.com>
2018-12-24 11:34:30 +0800

    Initial commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author HCX
+ * @Date 2018/12/24 11:29
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello github!");
+    }
+}
