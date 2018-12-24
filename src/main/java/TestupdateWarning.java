356d50f3c0d87fbbc7aabbc578f4eab8ade850d3
HCX <985701799@qq.com>
2018-12-24 11:46:05 +0800

    上传warning文件

---
+++ src/main/java/TestupdateWarning.java
@@ -0,0 +1,21 @@
+/**
+ * @ClassName TestupdateWarning
+ * @Description TODO
+ * @Author HCX
+ * @Date 2018/12/24 11:44
+ * @Version 1.0
+ **/
+public class TestupdateWarning {
+    /**
+    * @Author HCX
+    * @Description //TODO
+    * @Date 11:44 2018/12/24
+    * @param args
+    * @return void
+    * @exception
+    **/
+
+    public static void main(String[] args) {
+        System.out.println("update waring!");
+    }
+}
